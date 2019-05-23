2f4f73fd0adefdd2805d6949384115ffb16a2a0c
Lucas Miguel Martiniano <lucas.m.m@A309-12L.imd.local>
2019-05-23 12:24:19 -0300

    leitor csv

--- src/br/ufrn/imd/modelo/LeitorCSV.java
+++ src/br/ufrn/imd/modelo/LeitorCSV.java
@@ -1,5 +1,44 @@
 package br.ufrn.imd.modelo;
 
-public class LeitorCSV {
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 
+/*
+ * Leitor CSV 
+ * 
+ * Esta classe implementa os métodos necessários
+ * à abertura e leitura de um arquivo CSV
+ * 
+ */
+
+public class LeitorCSV {
+	private BufferedReader csv; // Objeto de leitura de arquivo
+	
+	public LeitorCSV(String caminho) {
+		try { // Tenta abrir o arquivo localizado em caminho
+			
+			csv = new BufferedReader(new FileReader(caminho)); // Instancia o objeto de leitura de arquivos
+			
+			
+			lerLinha(); // Ler o cabeçalho
+			
+		} catch (FileNotFoundException e) { // Captura exceção caso o arquivo não exista
+			
+			e.printStackTrace();
+			
+		}
+	}
+	
+	public String lerLinha() {
+		String linha = null;
+		
+		try {
+			linha = csv.readLine();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+		return linha;
+	}
 }
